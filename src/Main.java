157ed1e4e76919901a0c3db3cc442cedcbe4e020
Emily Johnson <ejohnsischeme@gmail.com>
2015-01-14 18:28:46 -0500

    Altered main so that one human player can play against one computer player; added comments about players

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//Player set up; can also be 2 humans or 2 computers; adding option as part of gameplay at later date
 		Player p1 = new CompPlayer();
 		Player p2 = new HumanPlayer();
 		
