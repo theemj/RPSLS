ffd4c8ad6c1c33edd3559a9e828f7a93f7db8844
Emily Johnson <ejohnsischeme@gmail.com>
2015-01-14 18:21:40 -0500

    Altered main ao that one human player can play against one computer player

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Player p1 = new CompPlayer();
-		Player p2 = new CompPlayer();
+		Player p2 = new HumanPlayer();
 		
 		RPSLSGame game = new RPSLSGame(p1, p2);
 		
